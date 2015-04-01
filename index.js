7fd0180173c6295e9ab743b27d466f2e8d7772cf
Tom Hummel <tphummel@gmail.com>
2015-03-31 22:11:30 -0700

    onroto: season standings working

--- index.js
+++ index.js
@@ -1,3 +1,100 @@
-require('newrelic');
-require('coffee-script');
-require('./lib');
+var http = require('http')
+var cheerio = require('cheerio')
+var moment = require('moment')
+
+function getUrl(url, cb) {
+  var req = http.get(url, function(res) {
+    var data = ''
+
+    res.on('data', function (chunk) {
+      data += chunk
+    })
+    res.on('end', function() {
+      cb(null, data)
+    })
+  })
+
+  req.on('error', cb)
+}
+
+function getStandingsDate(dom){
+  var headings = dom('font b').filter(function(i) {
+    var inner = dom(this).text()
+    return /STANDINGS/.test(inner)
+  })
+
+  var seasonHeading = headings[0]
+
+  var rawDate = dom(seasonHeading).text().match(/\d{2}\.\d{2}\.\d{2}/)
+  var prettyDate = moment(rawDate, 'MM.DD.YY').format('YYYY-MM-DD')
+
+  return prettyDate
+}
+
+function getSeasonStandings(dom){
+  var tables = dom("table [cellpadding='2']")
+  var seasonTable = tables[0]
+  var rows = dom(seasonTable).children('tr')
+
+  var keys = []
+  dom(rows)
+    .first()
+    .children('th')
+    .each(function(i){
+      keys.push(dom(this).text().trim())
+    })
+
+  var standings = []
+  dom(rows)
+    .slice(1)
+    .each(function(i){
+
+      var team = {
+        order: i
+      }
+
+      dom(this)
+        .children('td')
+        .each(function(j){
+          team[keys[j]] = dom(this).text()
+        })
+
+      standings.push(team)
+    })
+
+  return standings
+}
+
+function getWeekStandings(dom){
+  var tables = dom("table [cellpadding='2']")
+  var weeklyTable = tables[1]
+}
+
+function getCategoryStandings(dom){
+  var tables = dom("table [cellpadding='2']")
+  var catParentTable = tables[2]
+}
+
+function scrapeStandings(text, cb){
+  var dom = cheerio.load(text)
+
+  standingsDate = getStandingsDate(dom)
+
+  seasonStandings = getSeasonStandings(dom)
+
+  var result = {
+    standingsDate: standingsDate,
+    seasonStandings: seasonStandings
+  }
+
+  return cb(null, result)
+}
+
+function extractSeason(text, cb){
+
+}
+module.exports = {
+  scrapeStandings: scrapeStandings
+}
+
+
---
+++ test/index.js
@@ -0,0 +1,21 @@
+var fs = require('fs')
+var path = require('path')
+var test = require('tape')
+var lib = require('../')
+
+test('scrape test file', function(t){
+  var htmlFile = path.join(__dirname, 'fixtures/test.html')
+  fs.readFile(htmlFile, function(err, text){
+    t.equal(err, null)
+
+    lib.scrapeStandings(text, function(err, result){
+      t.equal(err, null)
+      t.equal(result.standingsDate, '2015-04-04')
+      t.equal(result.seasonStandings.length, 11)
+      t.end()
+    })
+  })
+
+
+
+})
